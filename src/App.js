3eae4b04d187b395d241f8a68fca48fb12871093
Françuel Soares <fs.francuelsoares@gmail.com>
2020-02-28 22:23:07 -0300

    0.2.0
    # New - Added useSate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [techs, setTech] = useState(['Node.js', 'ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...techs, newTech]);
+    setNewTech('');
+  }
+
   return (
-    <h1>Olá mundo!</h1>
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li>{tech}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        onChange={e => setNewTech(e.target.value)}
+        value={newTech}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
   );
 }
 
